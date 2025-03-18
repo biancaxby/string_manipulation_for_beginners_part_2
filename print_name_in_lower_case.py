303487e3a09dc6e0e984902ce0298685336211eb
biancaxby <paulabiancapiquero@gmail.com>
2025-03-19 03:00:07 +0800

    used the .lower funtion to convert the characters to lowercase

--- print_name_in_lower_case.py
+++ print_name_in_lower_case.py
@@ -1,1 +1,6 @@
 # Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+print('Enter your full name')
+my_name = input('Name: ')
+
+print(my_name.lower())
