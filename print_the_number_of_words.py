1931f2ed5c88f903958846971510c22d12910aa4
biancaxby <paulabiancapiquero@gmail.com>
2025-03-19 02:54:28 +0800

    used split funtion to split sentences together and then used len to count each word

--- print_the_number_of_words.py
+++ print_the_number_of_words.py
@@ -1,1 +1,7 @@
 # Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+print('Enter something that you want to say!')
+statement = input()
+split_sentence = statement.split()
+
+print(len(split_sentence))
