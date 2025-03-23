5d9d6090d11146161550019c8f7b97588fdba9d7
biancaxby <paulabiancapiquero@gmail.com>
2025-03-23 13:24:28 +0800

    used title function to convert the string into a title, which has capitalized first letters

--- input_name_in_incorrect_casing_and_print_the_proper_casing.py
+++ input_name_in_incorrect_casing_and_print_the_proper_casing.py
@@ -1,1 +1,8 @@
 # Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+# ask user for input 
+print('Enter your fullname with the incorrect casing and this program wi;; convert it into proper casing!')
+fullname = input('Enter name here: ')
+
+# use title function to convert the improper casing to a proper one
+print(fullname.title())
