5cffb3030e1a05e9856f3a312b23c4672a01d571
biancaxby <paulabiancapiquero@gmail.com>
2025-03-23 13:52:48 +0800

    used lstrip to remove the leading characters (which is the spaces) to return it without the spaces in the left

--- remove_spaces_in_the_beginning_of_the_names.py
+++ remove_spaces_in_the_beginning_of_the_names.py
@@ -1,1 +1,8 @@
 # Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# ask user for input
+print('Enter your fullname with a LOT of spaces in the beginning and this program will remove the spaces.')
+name = input('Enter your name here: ')
+
+# remove the spaces on the beginning of the name
+print(name.lstrip())
