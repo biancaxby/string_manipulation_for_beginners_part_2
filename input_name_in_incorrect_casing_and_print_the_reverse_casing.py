a6ee2eab4662d9db7fece969aa3b940cf1cebbb5
biancaxby <paulabiancapiquero@gmail.com>
2025-03-23 13:30:06 +0800

    used swapcase to convert the casings

--- input_name_in_incorrect_casing_and_print_the_reverse_casing.py
+++ input_name_in_incorrect_casing_and_print_the_reverse_casing.py
@@ -1,1 +1,7 @@
 # Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+# ask user for input
+print('Enter your name in an incorrect casing format and this program will reverse the casing!')
+name = input('Enter your name here: ')
+# convert the lowercase to uppercase and vice versa
+print(name.swapcase())
