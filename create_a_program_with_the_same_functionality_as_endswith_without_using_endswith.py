7836e45f86228a5483d1385d80ea42e94ca21f30
biancaxby <paulabiancapiquero@gmail.com>
2025-03-26 23:03:48 +0800

    used two different variables to distinguish the statement as well as the symbol or word that you want to find. i used -len function so that it starts finding the symbol on the end part of the statement.

--- create_a_program_with_the_same_functionality_as_endswith_without_using_endswith.py
+++ create_a_program_with_the_same_functionality_as_endswith_without_using_endswith.py
@@ -1,1 +1,12 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# Assign variables first
+statement = input('Say whatever you want: ')
+locate = input('What do you want to find?: ')
+
+# Use find or index to locate the ending symbol or word
+if statement[-len(locate)] == locate:
+    print(f"Found it! It's in the end of the statement!")
+
+else:
+    print(f'{locate} is not in the end')
