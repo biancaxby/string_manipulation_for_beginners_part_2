8390583e7a071a406f275552f8d49cbaf1e31772
biancaxby <paulabiancapiquero@gmail.com>
2025-03-26 21:39:38 +0800

    used rjust to add spaces so that the string is moved to the right.

--- create_a_program_with_the_same_functionality_as_center_without_using_center.py
+++ create_a_program_with_the_same_functionality_as_center_without_using_center.py
@@ -1,1 +1,10 @@
 #Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# Assign values to the variables
+sentence = "Witness the might of the seas!"
+
+# Add spaces on the left to make the value on the center
+center_substitute = sentence.rjust(65)
+
+# Print the output
+print(center_substitute)
