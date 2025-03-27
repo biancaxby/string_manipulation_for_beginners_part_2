fdfeb9a36ead8f21a45a1be4a8b953d364c2cc34
biancaxby <paulabiancapiquero@gmail.com>
2025-03-27 11:42:27 +0800

    used list comprehension. i thought of using split and then use capitalize but then it doesnt work since its a list and not a string so i had to used list comprehension instead.

--- create_a_program_with_the_same_functionality_as_title_without_using_title.py
+++ create_a_program_with_the_same_functionality_as_title_without_using_title.py
@@ -1,1 +1,12 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# Ask user for input
+statement = input("Enter whatever you want to say: ")
+
+# Separate the sentences 
+strip_statement = statement.split()
+# Locate the first letter per word in the list 
+capital = [word[0].upper() + word[1:].lower() if len(word) > 0 else "" for word in strip_statement]
+
+
+print(" ".join(capital))
