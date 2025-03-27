004c2775dcb4d781192e765199d9669af45439a7
biancaxby <paulabiancapiquero@gmail.com>
2025-03-27 13:11:48 +0800

    tried spliting each characters to check if each letters are capital, but it cannot be since its a list

--- create_a_program_with_the_same_functionality_as_isupper_without_using_isupper.py
+++ create_a_program_with_the_same_functionality_as_isupper_without_using_isupper.py
@@ -1,1 +1,12 @@
 #Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+# Ask user for input
+sentence = input('Enter a sentence: ')
+
+# Check if the input is all in capital letters
+# Print 'True' if the input is all in capital letters
+if sentence == sentence.upper():
+    print('True')
+
+else:
+    print('False')
