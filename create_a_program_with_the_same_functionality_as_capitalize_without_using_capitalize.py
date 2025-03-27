9ee495e6ec37720fd2fdf2342593850241a86ab7
biancaxby <paulabiancapiquero@gmail.com>
2025-03-27 19:04:07 +0800

    separated the first letter in the string to convert it to uppercase then the rest of the string to lowercase. after that, the two variables are then joined together

--- create_a_program_with_the_same_functionality_as_capitalize_without_using_capitalize.py
+++ create_a_program_with_the_same_functionality_as_capitalize_without_using_capitalize.py
@@ -1,1 +1,15 @@
 # Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+# Ask user for input
+print('This program will mimic the function of a capitalize function without using it')
+statement = input('Say whatever you want: ')
+
+# Locate the first letter of the string and convert it to uppercase
+capitalize = statement[:1].upper()
+# Convert the rest of the string to lowercase
+characters = statement[1:].lower()
+
+# Concatinate the two variables together
+joined_variables = capitalize + characters
+
+print(joined_variables)
