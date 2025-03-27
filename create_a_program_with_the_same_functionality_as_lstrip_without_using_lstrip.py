af1c339a4b37ae577f72307069b9f18ccae3bf39
biancaxby <paulabiancapiquero@gmail.com>
2025-03-27 16:05:07 +0800

    used split to separate each word then used join to combine them again to remove the other spaces in the beginning

--- create_a_program_with_the_same_functionality_as_lstrip_without_using_lstrip.py
+++ create_a_program_with_the_same_functionality_as_lstrip_without_using_lstrip.py
@@ -1,1 +1,14 @@
-#Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+# Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# Ask user for input
+print('This program will mimic the lstrip() funtion without using lstrip()')
+sentence = input('Enter something: ')
+# Split the statement 
+my_list = []
+split_sentence = sentence.split
+append_list = my_list.append(split_sentence)
+
+# Join or combine the statement
+join_sentence = ' '.join(append_list)
+print(join_sentence)
+
