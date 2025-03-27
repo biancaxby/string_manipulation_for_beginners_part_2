94e97a1275d3c77f3be1b7d8c4978cf7fca8e238
biancaxby <paulabiancapiquero@gmail.com>
2025-03-27 17:58:55 +0800

    used replace function to replace the prefix that wanted to be removed by the user.

--- create_a_program_with_the_same_functionality_as_removeprefix_without_using_removeprefix.py
+++ create_a_program_with_the_same_functionality_as_removeprefix_without_using_removeprefix.py
@@ -1,1 +1,12 @@
 #Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+# Ask user for input
+print('This program will mimic the funtion removeprefix( w/o using it)')
+user_input = input('Give me a word or something: ')
+# Ask them what to remove
+prefix = input('What do you want to remove?: ')
+# Replace the values that they want to remove
+if user_input:
+    replaced = user_input.replace(prefix, "")
+
+print(replaced)
