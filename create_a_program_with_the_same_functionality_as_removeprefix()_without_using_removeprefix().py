c0d718bf91b9320421635c85be23f4b8b048e75b
Paula Bianca A. Piquero <paulabiancapiquero@gmail.com>
2025-03-28 22:45:40 +0800

    duplicate file

--- create_a_program_with_the_same_functionality_as_removeprefix()_without_using_removeprefix().py
+++
@@ -1,1 +0,0 @@
-#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
