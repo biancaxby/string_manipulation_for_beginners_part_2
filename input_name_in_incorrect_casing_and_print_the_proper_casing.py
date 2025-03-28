8f139323efaa67ca80db0cc94f24cf32b4a4b8b1
Paula Bianca A. Piquero <paulabiancapiquero@gmail.com>
2025-03-28 22:46:36 +0800

    Delete input_name_in_incorrect_casing_and_print_the_proper_casing.py

--- input_name_in_incorrect_casing_and_print_the_proper_casing.py
+++
@@ -1,8 +0,0 @@
-# Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
-
-# ask user for input 
-print('Enter your fullname with the incorrect casing and this program will convert it into proper casing!')
-fullname = input('Enter name here: ')
-
-# use title function to convert the improper casing to a proper one
-print(fullname.title())
