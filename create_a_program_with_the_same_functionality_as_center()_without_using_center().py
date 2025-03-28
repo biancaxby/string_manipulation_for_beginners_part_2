22bee949620526602251d8e61aec10898b369b68
Paula Bianca A. Piquero <paulabiancapiquero@gmail.com>
2025-03-28 22:32:40 +0800

    duplicate file

--- create_a_program_with_the_same_functionality_as_center()_without_using_center().py
+++
@@ -1,1 +0,0 @@
-#Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
