4cb21d9f1b0061fe7c6f51e68221fcfe53edcaf6
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 02:13:38 +0800

    used rjust to add zeros to the beginning of the string.

--- create_a_program_with_the_same_functionality_as_zfill.py
+++ create_a_program_with_the_same_functionality_as_zfill.py
@@ -1,1 +1,9 @@
 # Create a program with the same functionality as zfill
+
+# Ask user for input
+print('This program will mimic the function of zfill withput using it')
+user_input = input('Enter a number: ')
+
+# Add zeros to the beginning 
+zero_fill = user_input.rjust(10, "0")
+print(zero_fill)
