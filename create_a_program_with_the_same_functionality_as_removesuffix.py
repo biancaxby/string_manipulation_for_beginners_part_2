4b0e289c7ff1ce7bf6de68c2953c19cbf5cad218
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-30 17:02:28 +0800

    used replace to remove the end of the string

--- create_a_program_with_the_same_functionality_as_removesuffix.py
+++ create_a_program_with_the_same_functionality_as_removesuffix.py
@@ -1,1 +1,14 @@
 # Create a program that has the same funtionality as removesuffix
+
+# Ask user for input
+print('This program will mimic the function of removesuffix without using it')
+user_input = input('Say something: ')
+remove_this = input('What do you want to remove?: ')
+
+# Remove the what the user wants to remove
+if user_input.endswith(remove_this):
+    removed_suffix = user_input.replace(remove_this, "")
+    print(removed_suffix)
+
+else:
+    print('Error')
