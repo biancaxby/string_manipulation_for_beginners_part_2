9ab995c87ad12c8c2f94b61bf74473c3f8a1e1f6
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 02:21:42 +0800

    i used the same method i used in isupper. i used the condition user_input == user_input.lower to check if the input was all in lowercase.

--- create_a_program_with_the_same_functionality_as_islower.py
+++ create_a_program_with_the_same_functionality_as_islower.py
@@ -1,1 +1,12 @@
 # Create a program with the same functionality as islower
+
+# Ask user for input
+print('This program will mimic the function of islower without using it')
+user_input = input('Say something: ')
+
+# Check if the input are all in lower case
+if user_input == user_input.lower():
+    print('All in lowercase')
+
+else:
+    print('They are not all in lower case') 
