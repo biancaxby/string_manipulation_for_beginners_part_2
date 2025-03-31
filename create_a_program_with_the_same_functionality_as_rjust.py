dd77926d22698580f778e1955d70871d67190529
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 17:43:38 +0800

    used format to add spaces in the beginning of the string

--- create_a_program_with_the_same_functionality_as_rjust.py
+++ create_a_program_with_the_same_functionality_as_rjust.py
@@ -1,1 +1,10 @@
 # Create a program with the same functionality as rjust 
+
+# Ask user for input
+print('This program will mimic the funtion of rjust without using it')
+user_input = input('Say something: ')
+
+# Format the string to add spaces on the beginning of the string
+formatted_string = "{:>20}".format(user_input)
+
+print(formatted_string)
