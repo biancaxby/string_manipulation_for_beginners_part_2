cdf8aaf164dc5706a3f2170ae055710977c07618
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 16:57:47 +0800

    used find since it has the same funtion as iindex

--- create_a_program_with_the_same_functionality_as_index.py
+++ create_a_program_with_the_same_functionality_as_index.py
@@ -1,1 +1,14 @@
 # Create a program with the same functionality as index
+
+# Ask user for input
+print('This program will mimic the function of index without using it')
+user_input = input('Say something: ')
+find_word = input('What do you want to find?: ')
+
+# Locate the first appearance of the word
+if find_word in user_input:
+    word_finder = user_input.find(find_word)
+    print(f'The word you are looking for is in {word_finder}')
+
+else:
+    print('The word you are looking for is not here')
