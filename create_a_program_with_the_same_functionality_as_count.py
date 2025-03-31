442a610d241350b527f23eb381d5b12bd7796a6d
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 16:30:59 +0800

    separated the words to turn it into a list and then counted every time the word appeared in the string

--- create_a_program_with_the_same_functionality_as_count.py
+++ create_a_program_with_the_same_functionality_as_count.py
@@ -1,1 +1,19 @@
 # Create a program with the same functionality as count
+
+# Initialize variables
+word_counter = 0
+
+# Ask user for input
+print('This program shall mimic the function of count')
+user_input = input('Say something: ')
+count = input('What do you want to count?: ')
+
+# Separate the words 
+split_words = user_input.split()
+for word in split_words:
+    if word == count :
+        word_counter += 1  
+
+   
+
+print((word_counter))
