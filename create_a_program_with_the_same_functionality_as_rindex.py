b779c0c5e707252a804986db1585602c0251456e
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 15:24:23 +0800

    used rfind to find the word starting from the beginning og the string

--- create_a_program_with_the_same_functionality_as_rindex.py
+++ create_a_program_with_the_same_functionality_as_rindex.py
@@ -1,1 +1,14 @@
 # Create  a program with the same functionality as rindex
+
+# Ask user for input
+print('This program will mimic the function of rindex without using it')
+user_input = input('Say something: ')
+word_find = input('What do you want to find?: ')
+
+# Find the last apperance of the word
+if word_find in user_input:
+    last_occurence = user_input.rfind(word_find)
+    print(f'The word you were looking for last appeared at {last_occurence}')
+
+else: 
+    print('It seems like the word you are looking for is not here')
