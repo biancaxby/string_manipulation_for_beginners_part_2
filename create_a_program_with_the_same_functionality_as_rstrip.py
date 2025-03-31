194ec1d7dda81dacad67ae8ba2dbfaf0510509e0
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 09:19:18 +0800

    used replace function to remove the spaces at the end

--- create_a_program_with_the_same_functionality_as_rstrip.py
+++ create_a_program_with_the_same_functionality_as_rstrip.py
@@ -1,1 +1,9 @@
 # Create a program with the same functionality as rstrip
+
+# Ask user for input
+user_input = input('Say something: ')
+
+# Remove the spaces in the end
+remove = user_input.replace("  ", "")
+
+print(remove)
