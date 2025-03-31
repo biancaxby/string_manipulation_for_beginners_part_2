cbcb94886ba1c5c1e7ed0df6019c8dc08db39476
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 17:04:19 +0800

    converted the string to lowercase then used swapcase to convert the lowercase to uppercase

--- create_a_program_with_the_same_functionality_as_upper.py
+++ create_a_program_with_the_same_functionality_as_upper.py
@@ -1,1 +1,10 @@
-# Create a program with the same funtionality as as upper
+# Create a program with the same funtionality as upper
+
+# Ask user for input
+print('This program will mimic the function of upper without using it')
+user_input = input('say something: ')
+
+# Convert all of the characters to lowercase then use swapcase
+lower_case = user_input.lower()
+
+print(lower_case.swapcase())
