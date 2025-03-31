7d3dbb4b1f9f78d8666203e2e904d395cf964707
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-03-31 09:25:32 +0800

    tried using the same method i did with endswith but it doesnt work as intended so ill try finding another way

--- create_a_program_with_the_same_functionality_as_startswith.py
+++ create_a_program_with_the_same_functionality_as_startswith.py
@@ -1,1 +1,12 @@
 # Create a program with the same functionality as startswith
+
+# Ask user for input
+user_input = input('Say something: ')
+start = input('Your statement starts wiht?: ')
+
+# Check if the second input is in the first input
+if user_input[len(start)] == start:
+    print(f'It indeed starts with {start}')
+
+else:
+    print(f'Hmmm... It doesnt appear that the string starts with {start}')
